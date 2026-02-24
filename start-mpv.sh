a24f6d2d3487fea65d75ad7a5b97b2e86d21d54b
Manolo Müller <manolo.mueller@gmail.com>
2026-02-24 17:22:09 +0100

    fixed braindead mistake

--- start-mpv.sh
+++ start-mpv.sh
@@ -11,7 +11,7 @@ done | head -n1)
 # Fallback if nothing was found
 CONNECTED="${CONNECTED:-"HDMI-A-1"}"
 SOCKET="${SOCKET:-"/tmp/mpvsocket"}"
-PLAYLIST="${PLAYLIST:-"$USER/videos/playlist.txt"}"
+PLAYLIST="${PLAYLIST:-"$HOME/videos/playlist.txt"}"
 
 exec mpv \
     --input-ipc-server=$SOCKET \
