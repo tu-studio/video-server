3fa00e46f89b13e9fac24453c10e3974376c55f2
Manolo Müller <manolo.mueller@gmail.com>
2026-02-24 15:55:59 +0100

    fixed psychologically devastating missing newline bug

--- showcontrol-mock.py
+++ showcontrol-mock.py
@@ -4,16 +4,17 @@ import time
 # mockup simulating schedcontrol.py in showcontrol project
 
 IP = "130.149.23.119"
+PORT = 9001
 
 def wrap_cmd(cmd, args={}):
     args["command"] = cmd
     return args
     # return {"command": cmd}
 
-def send_udp_broadcast(cmd, ip=IP, port=9000, args={}):
+def send_udp_broadcast(cmd, ip=IP, port=PORT, args={}):
     cmd = wrap_cmd(cmd, args)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    msg = json.dumps(cmd).encode("utf-8")
+    msg = json.dumps(cmd).encode("utf-8") + b"\n"
     sock.sendto(msg, (ip, port))
     sock.close()
 
@@ -26,6 +27,9 @@ def resume():
 def load(path):
     send_udp_broadcast(["loadfile", path, "replace"])
 
+def get_playback_time():
+    send_udp_broadcast(["get_property", "playback-time"])
+
 def play_video(index, start_paused=False):
     # Set all video players to the correct video
     send_udp_broadcast(["playlist-play-index", index])
@@ -35,7 +39,8 @@ def play_video(index, start_paused=False):
         send_udp_broadcast(["set_property", "pause", "no"], args={"async": True})
 
 
-load("/home/pi/vid.mp4")
-# resume()
-# pause()
+# get_playback_time()
+# load("/home/pi/vid.mp4")
+resume()
+pause()
 # play_video(2)
